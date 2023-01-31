3f4bc4380c233f602d041c65755aaab6b7ea4663
Kee <shikee.maloney@codeimmersives.com>
2023-01-31 18:12:09 -0500

    all

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const logger = require('morgan');
 //importing routes from /routes folder
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const blogsRouter = require('./routes/blogs')
 
 const app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //we bind (associate) the routers to routes in app
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/blogs',blogsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
